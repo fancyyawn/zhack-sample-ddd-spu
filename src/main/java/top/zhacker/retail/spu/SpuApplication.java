e6ddadb77252ea39b400f6eb4fb6da9f9e29bd02
zhanghecheng <zhanghecheng@youzan.com>
2018-11-04 19:21:10 +0800

    add eureka

--- src/main/java/top/zhacker/retail/spu/SpuApplication.java
+++ src/main/java/top/zhacker/retail/spu/SpuApplication.java
@@ -2,14 +2,15 @@ package top.zhacker.retail.spu;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 
-// @EnableDiscoveryClient
+@EnableDiscoveryClient
 @SpringBootApplication
-public class SpuApplication {
+public class SpuApplication{
 
   public static void main(String[] args) {
     SpringApplication.run(SpuApplication.class, args);
   }
-  
+
 }
